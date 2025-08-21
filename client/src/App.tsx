416e7d5e5c715021528b7826cdce5df4fca4a197
Pallavi <pallavidule@gmail.com>
2025-08-21 14:25:08 +0200

    Basic landing page and other component creation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import OnboardingPage from './pages/OnboardingPage';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
+function App() {
   return (
-    <label> Hello</label>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage />}/>
+        <Route path='/register' element={<OnboardingPage />}/>
+        <Route path='/dahsboard' element={<Dashboard/>}/>
+        <Route path='profile' element={<Profile/>} />
+      </Routes>
+    </Router>
   )
 }
 
